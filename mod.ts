b3515a8cab1220b39899960a4259b66de30b7643
dkuroi <kaerit@hotmail.com>
2021-05-20 18:04:06 +0100

    mejoras estéticas

--- mod.ts
+++ mod.ts
@@ -1,24 +1,28 @@
-import { Application } from "./src/deps.ts"; const app = new Application();
+import { Application, oakCors } from "./src/deps.ts"; const app = new Application();
 //import { authorized } from "./authorized.ts";
 
+console.log();
+var t0 = performance.now();
+var projectName = `deno-api-rest`;
+var versioning = `0.1.0`;
+
 // ————————————————————————————————————————————————— DATOS DEL SERVIDOR
+console.log(`> ${projectName}@${versioning} mounting data`);
 const env = Deno.env.toObject();                     // Obtenemos objeto variables de entorno
-const HOST = '127.0.0.1' || env.HOST;                // IP del servidor
+const HOST = '0.0.0.0' || env.HOST;                  // IP del servidor
 const PORT = 8081 || Number(env.PORT) ;              // Puerto
 
 
 // ————————————————————————————————————————————————— MIDDLEWARES
-//app.use(organ());
-//app.use(oakCors());
+console.log(`> ${projectName}@${versioning} applying middlewares`);
+app.use(oakCors());
 
 
 // ————————————————————————————————————————————————— RUTAS 
-import RootRouter from './src/routes/root.ts';
-import UserRouter from './src/routes/users.ts';
-
+console.log(`> ${projectName}@${versioning} adding routes`);
 [                                                    // Incluír rutas
-    RootRouter,
-    UserRouter
+    await import('./src/routes/root.ts').then(module => module.default),
+    await import('./src/routes/users.ts').then(module => module.default)
 ].map(route => {                                     // por cada ruta:
     app.use(route.routes());                         // Usar las rutas creadas 
     app.use(route.allowedMethods());                 // POST, DELETE, GET
@@ -26,5 +30,11 @@ import UserRouter from './src/routes/users.ts';
 
 
 // ————————————————————————————————————————————————— STARTING THE SERVER
-console.log(` 🦕 Escuchando en http://${HOST}:${PORT}`);
+var t1 = performance.now()
+console.log('\n\x1b[42m\x1b[30m',' DONE ','\x1b[0m\x1b[32m', ` Success starting the server ${(t1-t0).toFixed(6)}ms\n`,'\x1b[0m');
+console.log('  🦕 Server running at:');
+console.log(`  - Local:`, '\x1b[33m',`\thttp://localhost:${PORT}`,'\x1b[0m');
+console.log(`  - Network:`, '\x1b[33m',`\thttp://${HOST}:${PORT}`,'\x1b[0m');
+console.log("\n");
+
 await app.listen({ port: PORT, hostname: HOST });
